4698c98b64f8dce427ef10c3e607e3ab24430f65
Madeleine Eastwood <166740418+maddy-eastwood@users.noreply.github.com>
2025-10-18 15:39:45 -0400

    Updates

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,1 +1,11 @@
+from typing import Annotated
+from fastapi import FastAPI
+from pydantic import BaseModel
 
+class User(BaseModel):
+    username: str
+    password: str
+
+class UserDB(User):
+    id: int
+    password_hash: str
