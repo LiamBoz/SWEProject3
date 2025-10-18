5eae9fbe2e3f2ed58704811e91359d4036b2036c
LiamBoz <thisamazingnow@gmail.com>
2025-10-18 15:37:55 -0400

    initialize session

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,1 +1,10 @@
-#todo : connect to db with sqlalchemy and create_engine
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+import os
+
+engine = create_engine(os.getenv("DATABASE_URL"), echo=False)
+
+Session = sessionmaker(bind=engine)
+
+class Base(DeclarativeBase):
+    pass
