83243f89d8fe19d77c1879856b3b3142ce055fa0
Madeleine Eastwood <166740418+maddy-eastwood@users.noreply.github.com>
2025-10-19 12:24:40 -0400

    Added configuration management through a pydantic model Settings class

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,10 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
-import os
+from app.core.config import settings
 
-engine = create_engine("postgresql://admin:password@postgres:5432/recipes_db", echo=False)
+# low level connection factory used to connect to our database
+engine = create_engine(settings.database_url, echo=False)
 
-Session = sessionmaker(bind=engine)
+# a session factory that is callable and returns new Session objects
+SessionLocal = sessionmaker(bind=engine)
 
+# the class to inherit from to create ORM models
 class Base(DeclarativeBase):
     pass
