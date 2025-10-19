840eda9ca9f7a9dc216b4001726421d8de7bc013
kathrynpete <kathrynpeters17@gmail.com>
2025-10-18 21:42:28 -0400

    Login input validation
    Now verifies if username and password meet requirements.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,51 +5,79 @@ function App() {
  
 
   return (
-    
         <LoginInput />
   );
 }
 
 function LoginInput() {
-  const [ loginInput, setLoginInput ] = useState('');
+  const [ usernameInput, setUsernameInput ] = useState('');
   const [ passwordInput, setPasswordInput ] = useState('');
-  const [ showUsername, setShowUsername] = useState(false);
+
   const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
 
-  function saveInputText(event){
-    setLoginInput(event.target.value);
+  const [ showUsername, setShowUsername] = useState(false);
+  const [ loginActivated, setLoginActivated ] = useState(false);
+
+
+  function saveUsernameText(event){
+    //saving username text
+    setUsernameInput(event.target.value);
   }
 
   function savePasswordText(event){
+    //saving password text
     setPasswordInput(event.target.value);
   }
 
   function login(){
-    setUsername(loginInput);
+    //saving login input to state variables username and password
+    setUsername(usernameInput);
     setPassword(passwordInput);
+
+    //username can only contain letters upper or lowercase
+    const validUsernameChars =  /^[A-Za-z]+$/;
+    /*
+      Password must meet following requirements:
+        Must be 8 characters long
+        Must include at least one of each:
+          Uppercase letter
+          Lowercase letter
+          Number
+          Special symbol
+    */
+    const validPasswordRule = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()\-+={}[\]:;"'<>,.?/\\|~`]).{8,}$/;
+
+    {validUsernameChars.test(usernameInput) && validPasswordRule.test(passwordInput) ? setShowUsername(true) : setShowUsername(false)}
+    
+    //If login is clicked, error messages can be shown
+    setLoginActivated(true);
+
+    //clears input from search bars
     setPasswordInput('');
-    setLoginInput('');
-    setShowUsername(true);
+    setUsernameInput('');
   }
 
   return(
     <div className="welcome">
+       {/*Welcome Paragraph*/}
         <p className="welcome-message">
           Welcome to Chopify
         </p>
       {/*<form onSubmit={handleInput}> */}
         <div className="login-bar">
+           {/*Username bar*/}
            <input
           type = "text"
           placeholder="Username"
-          onChange={saveInputText}
-          value={loginInput}
+          onChange={saveUsernameText}
+          value={usernameInput}
           className="username-input"
           />
           </div>
 
         <div className="login-bar">
+           {/*Password bar*/}
             <input 
               type="password"
               placeholder="Password"
@@ -58,7 +86,7 @@ function LoginInput() {
               className="password-input"
           />
           </div>
-
+        {/*Login button*/}
         <button 
         type="submit"
         onClick={login}
@@ -66,6 +94,17 @@ function LoginInput() {
             Login
           </button>
           {/* </form> */}
+
+         {/*Conditional paragraphs displayed*/}
+          {!showUsername &&
+            <p>
+            Username may only contain letters with no spaces, numbers, or special symbols.
+          </p>}
+
+          {!showUsername &&
+          <p>
+            Password must be 8 characters long, must include at least one uppercase letter, at least one lowercase letter, at least one number, and at least one special symbol.
+          </p>}
 
           {showUsername && username && password &&
           <p>
@@ -76,6 +115,22 @@ function LoginInput() {
           <p>
             My password is {password}  
           </p>}
+
+          {(!showUsername) && username && password &&
+            <p>
+            Error! Your username or password does not meet the requirements. Please try again!
+            </p>}
+            {(!username) && loginActivated &&
+            <p>
+              Error! Please enter your username.
+            </p>
+            }
+
+            {(!password) && loginActivated &&
+            <p>
+              Error! Please enter your password.
+            </p>
+            }
     </div>  
   );
 }
