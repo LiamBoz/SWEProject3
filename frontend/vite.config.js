1c1cf1ffc50dc79ccf83eb5b91c284aee110db70
LiamBoz <thisamazingnow@gmail.com>
2025-10-22 16:22:37 -0400

    add hot-reloading to frontend add tailwindcss config change app.jsx to .tsx so we can use typescript compomnents

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    }
+  },
   server: {
     // bind to all interfaces so the dev server is reachable from Docker host
     host: true,
