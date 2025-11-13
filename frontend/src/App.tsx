234ecec5e1665e91bbb6977fa68905ee44fcbe39
kathrynpete <kathrynpeters17@gmail.com>
2025-11-12 21:48:00 -0500

    Display Recipe function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import {BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { LoginInput } from "./pages/LoginInput.tsx";
 import { Homepage } from "./pages/Homepage.tsx";
-import { DisplayRecipe } from "./pages/DisplayRecipe.tsx";
+import { DisplayRecipeHook } from "./pages/DisplayRecipe.tsx";
 import { Toaster } from "react-hot-toast";
 
 function App() {
@@ -12,11 +12,7 @@ function App() {
           <Route path="/homepage" element={<Homepage />} />
           <Route path="*" element={<Navigate to="/" replace />} />
           {/* will change routing later */}
-          <Route path="/recipe" element={<DisplayRecipe
-            recipeTitle={"Sample Recipe"}
-            recipeInstructions={"1. Do this.\n2. Do that."}
-            recipeIngredients={"- Ingredient 1\n- Ingredient 2"}
-          />} />
+          <Route path="/recipe" element={<DisplayRecipeHook />} />
         </Routes> 
         <Toaster position="top-center" reverseOrder={false} />
       </Router>
--- frontend/src/pages/DisplayRecipe.tsx
+++ frontend/src/pages/DisplayRecipe.tsx
@@ -1,14 +1,90 @@
 // @ts-ignore: missing module declaration
 import "./LoginInput.css";
 
-export function DisplayRecipe({ recipeTitle, recipeInstructions, recipeIngredients }: { recipeTitle: string; recipeInstructions: string; recipeIngredients: string }){
+export function DisplayRecipeHook(){
+  //call hook
+  //call DisplayRecipe with recipe data
+
+
+}
+
+export function DisplayRecipe({ recipe } : { recipe: any }){
+  /*
+  recipe_name
+  total_time
+  prep_time
+  cook_time
+  rating
+  img_src
+  servings
+  ingredients
+  directions - by period senetnces
+  nutrition (csv)
+  overview - with additional time, csv
+  */
+
+  function parseDirections(directions: any){
+    let directions_str = "";
+    for(const key in directions){
+      if(key == '.'){
+        directions_str += key + "\n";
+      }else{
+        directions_str += key;
+      }
+      }
+      return directions_str;
+    }
+
+    function parseCSV(csv: any){
+      let paragraph = "";
+      for(const item in csv){
+        if(item == ','){
+          paragraph += "\n";
+        }else{
+          paragraph += item;
+        }
+      }
+      return paragraph;
+    }
+
+  
+ 
   return (
     <div>
-      <h2>{recipeTitle}</h2>
-      <h3>Ingredients:</h3>
-      <p>{recipeIngredients}</p>
-      <h3>Instructions:</h3>
-      <p>{recipeInstructions}</p>
+
+      <h2>{recipe.recipe_name}</h2>
+
+      {recipe.overview != "NaN" &&
+      <p>Overview: {parseCSV(recipe.overview)}</p>
+      }
+
+      {recipe.total_time != "NaN" && 
+      <p>Total Time: {recipe.total_time}</p>
+      }
+      {recipe.prep_time != "NaN" && 
+      <p>Prep Time: {recipe.prep_time}</p>
+      }
+      {recipe.cook_time != "NaN" && 
+      <p>Cook Time: {recipe.cook_time}</p>
+      }
+
+       {recipe.servings != "NaN" && 
+      <p>Servings: {recipe.servings}</p>
+      }
+
+      {recipe.rating != "NaN" && 
+      <p>Rating: {recipe.rating} / 5</p>
+      }
+
+      {recipe.nutrition != "NaN" && <p>Nutrition: {parseCSV(recipe.nutrition)}</p>}
+
+      {recipe.img_src != "NaN" && 
+      <img src={recipe.img_src} alt={recipe.recipe_name} />
+      }
+
+    <p>Ingredients: {recipe.ingredients}</p>
+
+    <p>Directions: {parseDirections(recipe.directions)}</p>
     </div>
   );
 }
