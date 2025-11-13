d1247fdf578e70313e5c2d182cf29918bd93d763
kathrynpete <kathrynpeters17@gmail.com>
2025-11-13 17:11:09 -0500

    Display recipe css updated
    updated the display recipe page to have prettier css

--- frontend/src/components/ui/icons/heroicons-heart.tsx
+++ frontend/src/components/ui/icons/heroicons-heart.tsx
@@ -19,12 +19,13 @@ export function HeartIcon({
       width={size}
       height={size}
       viewBox="0 0 24 24"
-      fill={favorite ? "red" :"none"}
+      fill={favorite ? "rgb(252, 0, 105)" :"none"}
       stroke={color}
       strokeWidth={strokeWidth}
       strokeLinecap="round"
       strokeLinejoin="round"
       className={className}
+      cursor="pointer"
       {...props}
     >
       <path d="M21 8.25c0-2.485-2.099-4.5-4.687-4.5c-1.936 0-3.598 1.126-4.313 2.733c-.715-1.607-2.377-2.733-4.312-2.733C5.098 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12"/>
